efdcabf5d84593b17b5084fb6cbbb3247dd3ae1d
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-09-15 12:36:35 +0200

    feat: add background music to the main menu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,9 +1,13 @@
+import backgroundMusic from "../assets/sounds/main-menu-background-music.mp3";
 import "../styles/MainMenu.css";
+import SoundSwitch from "./SoundSwitch";
 
 function MainMenu({ onClickPlay }) {
   return (
     <div className="main-menu">
       <div className="main-menu__elems-container">
+        <SoundSwitch soundURL={backgroundMusic} />
+
         <h1 className="main-menu__title">THE POTIONS OF SIMON</h1>
 
         <button
